3563ca16e721ef0346cf5c8f9bf66845149b64b3
Aviwani1234 <viratwani22@gmail.com>
2024-02-16 00:05:41 +0530

    .

--- DAY-05/Merge_Sort.cpp
+++ DAY-05/Merge_Sort.cpp
@@ -12,35 +12,36 @@ void merge(int *arr, int s, int e)
     int *second = new int[len2];
 
     int k = s;
-    for(int i=0;i<len1;i++)
+    for (int i = 0; i < len1; i++)
     {
         first[i] = arr[k++];
     }
 
-    k = mid+1;
-    for(int i=0;i<len2;i++)
+    k = mid + 1;
+    for (int i = 0; i < len2; i++)
     {
         second[i] = arr[k++];
     }
 
-    int i=0,j=0,index=s;
-    while(i<len1 && j<len2)
+    int i = 0, j = 0, index = s;
+    while (i < len1 && j < len2)
     {
-        if(first[i]<second[j])
+        if (first[i] < second[j])
         {
             arr[index++] = first[i++];
         }
-        else{
+        else
+        {
             arr[index++] = second[j++];
         }
     }
 
-    while(i<len1)
+    while (i < len1)
     {
         arr[index++] = first[i++];
     }
 
-    while(j<len2)
+    while (j < len2)
     {
         arr[index++] = second[j++];
     }
@@ -56,7 +57,7 @@ void mergeSort(int *arr, int s, int e)
     int mid = (s + e) / 2;
 
     mergeSort(arr, s, mid);
-    mergeSort(arr, mid+1, e);
+    mergeSort(arr, mid + 1, e);
 
     merge(arr, s, e);
 }
@@ -65,10 +66,12 @@ int main()
 {
     int n = 5;
     int arr[5] = {2, 1, 3, 5, 4};
+
     mergeSort(arr, 0, n - 1);
-    for(int i=0;i<n;i++)
+
+    for (int i = 0; i < n; i++)
     {
-        cout<<arr[i]<<" ";
+        cout << arr[i] << " ";
     }
 
     return 0;
